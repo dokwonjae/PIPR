b3d59b0710cd0dbb914aac43e35f97920660897f
dokwonjae <dobear7@naver.com>
2023-10-31 17:53:55 +0900

    10/31

---
+++ 01_JPA/01_easy_thema/02_creative-gh/frontend-typescript/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './components/common/NotFound';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+      {/* home 페이지 */}
+      <Route path="/" element={<Home/>} />
+      <Route path="*" element={<NotFound/>} />
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
